b38ea8766652241da02eb0e8bc9f2c87625074d4
pinguinonice <pinguinonice@me.com>
2019-05-03 16:19:47 +0200

    gifs

--- matlab/fiberconnection3d.m
+++ matlab/fiberconnection3d.m
@@ -11,7 +11,7 @@ if L>norm(f1-p1)+norm(f2-p1)||L>norm(f1-p2)+norm(f2-p2)
 end
 
 
-[x1,y1,z1]=speroid(f1,f2,L,100);
+[x1,y1,z1]=speroid(f1,f2,L,500);
     
       % finding closest point
 L2=sqrt((x1-p1(1)).^2+(y1-p1(2)).^2+(z1-p1(3)).^2)+sqrt((x1-p2(1)).^2+(y1-p2(2)).^2+(z1-p2(3)).^2);
@@ -19,7 +19,7 @@ L2=sqrt((x1-p1(1)).^2+(y1-p1(2)).^2+(z1-p1(3)).^2)+sqrt((x1-p2(1)).^2+(y1-p2(2))
       
 [L2 index]=min(L2);
 
-    [x2,y2,z2]=speroid(p1,p2,L2,100);
+    [x2,y2,z2]=speroid(p1,p2,L2,500);
     
 s=[x1(index),y1(index),z1(index)];
 
--- matlab/main3d.m
+++ matlab/main3d.m
@@ -6,14 +6,14 @@ close all
 
 S=[];
 figure
-%gif('L21.gif')
+gif('example2.gif')
 for i=0:0.1:100
-f1=[0,0,5];
-f2=[5,0,-5];
+f1=[0,0,9];
+f2=[5,0,14];
 L=norm(f1-f2)+i;
 
-p1=[0, 5,-5];
-p2=[5, 5,5];   
+p1=[0, 8,11];
+p2=[4, 5,12];   
     
 [s,x1,y1,z1,x2,y2,z2,L2] = fiberconnection3d(L,f1,f2,p1,p2);
 S=[S;s];
@@ -22,12 +22,13 @@ S=[S;s];
 
 hold off
 %plot3(x1,y1,z1,'b.')
-hold on
 
 %plot3(x2,y2,z2,'r.')
 
 plot3(S(:,1),S(:,2),S(:,3),'k.')
+hold on
 
+hold on
 plot3(f1(1),f1(2),f1(3),'bo')
 plot3(f2(1),f2(2),f2(3),'bo')
 plot3(p1(1),p1(2),p1(3),'ro')
@@ -37,10 +38,15 @@ plot3([f1(1) s(1) f2(1)],[f1(2) s(2) f2(2)],[f1(3) s(3) f2(3)],'b','LineWidth',1
 plot3([p1(1) s(1) p2(1)],[p1(2) s(2) p2(2)],[p1(3) s(3) p2(3)],'r','LineWidth',1)
 
 axis equal
-axis([-5 20 -10 20])
+xlim([-5 15])
+ylim([-5 15])
+zlim([3 20])
+
 grid on
+view([-50-i*5 41+i])
+
 drawnow
-%gif
+gif
 %pause(0.1)
 if L2-norm(p1-p2)<0.01
     break
